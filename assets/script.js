ec132936932cd72fe18861065cfe3f9207aa8c98
Ufuk ALTAY <32794293+UfukAltay@users.noreply.github.com>
2025-06-13 23:54:03 +0300

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+// Modal açma kapama
+const modal = document.getElementById("contactModal");
+const btn = document.getElementById("openModalBtn");
+const span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
